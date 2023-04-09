9da15b3f99a2e423be2a6acbf09f38b770b58181
Marcel Weber <mrweber@gmx.ch>
2023-04-09 17:14:17 +0200

    BAsic target state handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import Target from "./components/Target.tsx";
 import Screen from "./components/Screen.tsx";
 import Controller from "./components/Controller.tsx";
 
+import { useState } from "react";
+
+import { targetList } from "./targetList.tsx";
+
 import "./styles/globals.css";
 
 function App() {
+  const [target, setTarget] = useState<String>("300m_A");
+
   return (
     <main>
-      <Target />
+      <Target name={target} />
       <Screen />
       <Controller />
     </main>
