ea511174bb6cb47a6943707cc9f7de2fd43bd1f4
Marcel Weber <mrweber@gmx.ch>
2023-04-09 16:29:34 +0200

    Base scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Target from "./components/Target.tsx";
+import Screen from "./components/Screen.tsx";
+import Controller from "./components/Controller.tsx";
+
+import "./styles/globals.css";
+
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Target />
+      <Screen />
+      <Controller />
+    </main>
+  );
 }
 
 export default App;
