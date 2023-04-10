6c653e395ba0854b04052a0eee78e49ef57ad265
Marcel Weber <mrweber@gmx.ch>
2023-04-10 19:08:16 +0200

    Styling and layouting

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -35,28 +35,42 @@ export default function Controller({
   return (
     <section className={s.container}>
       <h1 className={s.title}>{`${weapon.designation}\n${target.name}`}</h1>
-      <div className={s.content}>
-        <div className={s.display}>
-          <div className={s.direction}>
-            {windageAdjust > 0
-              ? "bei links"
-              : windageAdjust < 0
-              ? "Bei rechts"
-              : "Mittig!"}
-          </div>
-          <div className={s.amount}>{Math.abs(windageAdjust)}</div>
+      {manualHitPosition.length === 0 ? (
+        <div className={s.placeholder}>
+          Auf die Scheibe tippen, um den Treffer festzulegen!
         </div>
-        <div className={s.display}>
-          <div className={s.direction}>
-            {elevationAdjust > 0
-              ? "bei hoch"
-              : elevationAdjust < 0
-              ? "Bei tief"
-              : "Mittig!"}
+      ) : (
+        <div className={s.content}>
+          <div className={s.display}>
+            <div className={s.direction}>
+              {windageAdjust > 0
+                ? "bei links"
+                : windageAdjust < 0
+                ? "Bei rechts"
+                : "Mittig!"}
+            </div>
+            <div className={s.amount}>{Math.abs(windageAdjust)}</div>
           </div>
-          <div className={s.amount}>{Math.abs(elevationAdjust)}</div>
+          <div className={s.display}>
+            <div className={s.direction}>
+              {elevationAdjust > 0
+                ? "bei hoch"
+                : elevationAdjust < 0
+                ? "Bei tief"
+                : "Mittig!"}
+            </div>
+            <div className={s.amount}>{Math.abs(elevationAdjust)}</div>
+          </div>
+          <div className={s.info}>
+            <div
+              className={s.steps}
+            >{`Verstellschritt seitlich: ${weapon.windageStep}cm`}</div>
+            <div
+              className={s.steps}
+            >{`Verstellschritt Höhe: ${weapon.elevationStep}cm`}</div>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
