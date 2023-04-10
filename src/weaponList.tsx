7506b877d4ce840677a2183c706a824216a875ee
Marcel Weber <mrweber@gmx.ch>
2023-04-10 18:47:53 +0200

    Added weapon list

---
+++ src/weaponList.tsx
@@ -0,0 +1,83 @@
+interface Weapon {
+  designation: string;
+  windageStep: number;
+  elevationStep: number;
+  base: number;
+}
+
+interface Weapons extends Array<Weapon> {}
+
+export const weaponList: Weapon[] = [
+  {
+    designation: "G - Sturmgewehr 90",
+    windageStep: 4.5,
+    elevationStep: 4.5,
+    base: 300,
+  },
+  {
+    designation: "G - Sturmgewehr 57/02",
+    windageStep: 6.0,
+    elevationStep: 8.0,
+    base: 300,
+  },
+  {
+    designation: "D - Wyss Flex",
+    windageStep: 1.0,
+    elevationStep: 1.0,
+    base: 300,
+  },
+  {
+    designation: "D - Anschütz 6805",
+    windageStep: 1.2,
+    elevationStep: 1.2,
+    base: 300,
+  },
+  {
+    designation: "D - W+F K31",
+    windageStep: 2.0,
+    elevationStep: 2.0,
+    base: 300,
+  },
+  {
+    designation: "P - Hämmerli SP20",
+    windageStep: 0.8,
+    elevationStep: 1.0,
+    base: 25,
+  },
+  {
+    designation: "P - Pardini SP",
+    windageStep: 0.5,
+    elevationStep: 0.5,
+    base: 25,
+  },
+  {
+    designation: "D - Dobler Mikrometer-Visier zu P210",
+    windageStep: 1.5,
+    elevationStep: 1.5,
+    base: 25,
+  },
+  {
+    designation: "ZF - 1/4 MOA",
+    windageStep: 2.54 / 4,
+    elevationStep: 2.54 / 4,
+    base: 91.44,
+  },
+  {
+    designation: "ZF - 1 MRAD",
+    windageStep: 1.0,
+    elevationStep: 1.0,
+    base: 100,
+  },
+  {
+    designation: "LP - Feinwerkbau Modell 65",
+    windageStep: 1.5,
+    elevationStep: 1.5,
+    base: 10,
+  },
+  {
+    designation: "LP - FAS 604",
+    windageStep: 2.5,
+    elevationStep: 3.0,
+    base: 10,
+  },
+];
