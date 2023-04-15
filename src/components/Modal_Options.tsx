cfbc849596f6760a703dc722c132f3101ae7bf52
Marcel Weber <mrweber@gmx.ch>
2023-04-15 15:23:26 +0200

    Reworked options menu to have clickable images instead of selects

--- src/components/Modal_Options.tsx
+++ src/components/Modal_Options.tsx
@@ -52,18 +52,34 @@ export default function Modal_Options({
   });
 
   function assignWeapon(event: any) {
+    const weaponImages = document.querySelectorAll(`.${s.weaponImage}`)
     for (let weapon of weaponList) {
-      if (weapon.designation === event.currentTarget.value) {
+      if (weapon.designation === event.currentTarget.id) {
         setWeapon(weapon);
         localStorage.setItem("Schützenhilfe_Waffe", JSON.stringify(weapon));
       }
     }
+    for(let weapon of weaponImages){
+      if(weapon.id === event.currentTarget.id){
+        weapon.classList.add("active")
+      } else {
+        weapon.classList.remove("active")
+      }
+    }
   }
   function assignTarget(event: any) {
+    const targetImages = document.querySelectorAll(`.${s.targetImage}`)
     for (let target of targetList) {
-      if (target.designation === event.currentTarget.value) {
+      if (target.designation === event.currentTarget.id) {
         setTarget(target);
         localStorage.setItem("Schützenhilfe_Ziel", JSON.stringify(target));
+      }
+    }
+    for(let target of targetImages){
+      if(target.id === event.currentTarget.id){
+        target.classList.add("active")
+      } else {
+        target.classList.remove("active")
       }
     }
   }
@@ -84,22 +100,95 @@ export default function Modal_Options({
           <CloseIcon />
         </button>
         <div className={s.content}>
-          <div className={s.item}>
-            <h2 className={s.title}>Waffe / Diopter</h2>
-            <select
-              name="weapons"
-              id="select_weapons"
-              value={weapon.designation}
-              onChange={(event: any) => assignWeapon(event)}
-            >
-              {weaponListSorted.map((weapon) => {
-                return (
-                  <option key={weapon.designation} value={weapon.designation}>
-                    {weapon.designation}
-                  </option>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Gewehre</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "G"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
                 );
+                  }
               })}
-            </select>
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Gewehrdiopter</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "D"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Pistolen</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "P"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Pistolenvisiere</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "V"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Luftpistolen</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "LP"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Zielfernrohre</h2>
+            <div className={s.itemGridInner}>            
+            {weaponList.map((weapon) => {
+              const prefix = weapon.designation.split(" - ")
+                if(prefix[0] === "ZF"){
+                  return (
+                  <div onClick={(event:any)=>assignWeapon(event)} key={weapon.designation} className={s.weaponImage} id={`${weapon.designation}`} style={{backgroundImage: `url("${weapon.designation.replaceAll("/", "_")}.jpg")`}}>
+                    <span className={s.targetName}>{prefix[1]}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
           </div>
           <div className={s.item}>
             <h2 className={s.title}>Distanz</h2>
@@ -111,22 +200,61 @@ export default function Modal_Options({
               onChange={(event: any) => assignDistance(event)}
             />
           </div>
-          <div className={s.item}>
-            <h2 className={s.title}>Zielscheibe</h2>
-            <select
-              name="targets"
-              id="select_targets"
-              value={target.designation}
-              onChange={(event: any) => assignTarget(event)}
-            >
-              {targetList.map((target) => {
-                return (
-                  <option key={target.designation} value={target.designation}>
-                    {target.name}
-                  </option>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Zielscheiben Pistole 50m</h2>
+            <div className={s.itemGridInner}>            
+            {targetList.map((target) => {
+                if(target.distance === 50 ){
+                  return (
+                  <div onClick={(event:any)=>assignTarget(event)} key={target.name} className={s.targetImage} id={`${target.designation}`} style={{backgroundImage: `url("${target.designation}.jpg")`}}>
+                    <span className={s.targetName}>{target.name}</span>
+                  </div>
                 );
+                  }
               })}
-            </select>
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Zielscheiben Pistole 25m</h2>
+            <div className={s.itemGridInner}>            
+            {targetList.map((target) => {
+                if(target.distance === 25 ){
+                  return (
+                  <div onClick={(event:any)=>assignTarget(event)} key={target.name} className={s.targetImage} id={`${target.designation}`} style={{backgroundImage: `url("${target.designation}.jpg")`}}>
+                    <span className={s.targetName}>{target.name}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Zielscheiben Gewehr 300m</h2>
+            <div className={s.itemGridInner}>            
+            {targetList.map((target) => {
+                if(target.distance === 300 ){
+                  return (
+                  <div onClick={(event:any)=>assignTarget(event)} key={target.name} className={s.targetImage} id={`${target.designation}`} style={{backgroundImage: `url("${target.designation}.jpg")`}}>
+                    <span className={s.targetName}>{target.name}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
+          </div>
+          <div className={s.itemGrid}>
+            <h2 className={s.title}>Zielscheiben Pistole 10m</h2>
+            <div className={s.itemGridInner}>            
+            {targetList.map((target) => {
+                if(target.distance === 10 ){
+                  return (
+                  <div onClick={(event:any)=>assignTarget(event)} key={target.name} className={s.targetImage} id={`${target.designation}`} style={{backgroundImage: `url("${target.designation}.jpg")`}}>
+                    <span className={s.targetName}>{target.name}</span>
+                  </div>
+                );
+                  }
+              })}
+              </div>
           </div>
         </div>
       </div>
