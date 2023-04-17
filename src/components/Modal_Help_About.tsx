53e273858e10266d08cf874835aad24991b19c4e
Marcel Weber <mrweber@gmx.ch>
2023-04-17 11:28:09 +0200

    Fleshed out ABout section

--- src/components/Modal_Help_About.tsx
+++ src/components/Modal_Help_About.tsx
@@ -29,8 +29,17 @@ export default function Modal_Help_About({
       </div>
       <div className={s.content}>
         <h1 className={s.title}>Über</h1>
+        <div className={s.links}>
+          <a href="https://paypal.me/schussfreude?country.x=CH&locale.x=de_DE" target="_blank">PayPal</a>
+          <a href="https://www.patreon.com/schussfreude" target="_blank">Patreon</a>
+          <a href="https://github.com/m00gendai/schuetzenhilfe-v2" target="_blank">GitHub</a>
+          <a href="https://ch.linkedin.com/in/marcel-weber-3a05a61bb" target="_blank">LinkedIn</a>
+          <a href="https://www.schussfreude.ch" target="_blank">schussfreude.ch</a>
+          <a href="https://www.waffenforum.ch" target="_blank">waffenforum.ch</a>
+        </div>
         <p className={s.copy}>
-          {`© 2022-${currentYear} Marcel Weber / schussfreude.ch`}
+          <span>{`© 2022-${currentYear} Marcel Weber / schussfreude.ch`}</span>
+          <a href="mailto:info@mrweber.ch">info@mrweber.ch</a>
         </p>
       </div>
     </>
