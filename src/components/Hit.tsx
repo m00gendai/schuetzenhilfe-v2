9e333ba56149382bf5432b3f17235d72ce12c4df
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2023-04-26 17:00:45 +0200

    Zoom option (#4)
    * Added magnifying glass icon
    
    * Created Zoom Button
    
    * Implemented zoom functionality on target image
    This required a wrapper div for the target to expand into and passing the zoom setting from screen.tsx to app.tsx to target.tsx and hit.tsx

--- src/components/Hit.tsx
+++ src/components/Hit.tsx
@@ -1,14 +1,29 @@
 import s from "../styles/Hit.module.css";
 
+import {useRef} from "react"
+
 interface hitProps {
   score: number;
   y: number;
   x: number;
+  zoom: number;
 }
 
-export default function Hit({ score, y, x }: hitProps) {
+export default function Hit({ score, y, x, zoom }: hitProps) {
+
+  const hitElement = useRef<HTMLDivElement>(null)
+
+  if (zoom === 1 && hitElement.current != null) {
+    hitElement.current.style.transform = 'scale(1,1) translate(-50%, -50%)';
+  }
+  if (zoom === 2 && hitElement.current != null) {
+    hitElement.current.style.transform = 'scale(0.5,0.5) translate(-50%, -50%)';
+  }
+  if (zoom === 3 && hitElement.current != null) {
+    hitElement.current.style.transform = 'scale(0.25,0.25) translate(-50%, -50%)';
+  }
   return (
-    <div className={s.container} style={{ top: `${y}px`, left: `${x}px` }}>
+    <div className={s.container} style={{ top: `${y}px`, left: `${x}px` }} ref={hitElement}>
       {score < 0 ? "0" : Math.ceil(score / 10)}
     </div>
   );
