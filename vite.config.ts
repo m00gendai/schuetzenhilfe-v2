e24fef09afc0483d763483ada6f385c1d668ebd2
Marcel Weber <mrweber@gmx.ch>
2023-05-10 10:45:54 +0200

    Manifest expanded

--- vite.config.ts
+++ vite.config.ts
@@ -16,22 +16,69 @@ export default defineConfig({
         },
         injectRegister: 'auto',
         includeAssets: ['src/assets/favicon.ico', '/Logo-192'],
+        manifestFilename: "manifest.json",
         manifest: {
           name: 'Schusshilfe',
           short_name: 'Schusshilfe',
           description: 'Die Hilfsapp für Schweizer Schützen',
           theme_color: '#f4d8a8',
+          display: "standalone",
+          lang: "de-CH",
+          dir: "rtl",
+          orientation: "portrait",
+          display_override: [
+            "standalone",
+            "browser",
+          ],
+          prefer_related_applications: false,
+          categories: [
+            "education", "entertainment", "reference", "sports", "utilities",
+          ],
           icons: [
             {
               src: 'Logo-192.png',
               sizes: '192x192',
-              type: 'image/png'
+              type: 'image/png',
+              purpose: "any",
             },
             {
               src: 'Logo-512.png',
               sizes: '512x512',
-              type: 'image/png'
+              type: 'image/png',
+              purpose: "maskable",
             }
+          ],
+          screenshots: [
+            {
+              src: "/Screenshots/sc_main1.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
+            {
+              src: "/Screenshots/sc_main2.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
+            {
+              src: "/Screenshots/sc_targets.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
+            {
+              src: "/Screenshots/sc_weapons.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
+            {
+              src: "/Screenshots/sc_factors.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
+            {
+              src: "/Screenshots/sc_help_sight.jpg",
+              sizes: "1080x2000",
+              type: "image/jpg",
+            },
           ]
         }
       }
